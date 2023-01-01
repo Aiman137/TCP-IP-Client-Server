2a6c584be2ded0a9b03c9326835bec100b52e490
Aiman137 <aimanlascaux@hotmail.com>
2023-01-01 18:26:23 +0100

    No se ha conseguidor hacer que el servidor responda a peticiones POST o PUT

--- http_client.py
+++ http_client.py
@@ -1,21 +1,30 @@
 import socket
 
+#Definimos la direccion del host y el puerto
 server_address = ('localhost', 8000)
+#Definimos el mensaje de la solicitud HTTP
+#Solicitud GET para obtener el recurso raiz
 message  = b'GET / HTTP/1.1\r\n'
+#Direccion del servidor
 message += b'Host: http://localhost:8000\r\n'
+#Despues de enviar la solicitud cerramos la conexion
 message += b'Connection: close\r\n'
 message += b'\r\n'
-
+#Creamos el socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Establecemos la conexion con el servidor
 sock.connect(server_address)
+#Enviamos la peticion al servidor
 sock.sendall(message)
 
+#En este bucle recibimos la respuesta del servidor
+#en bloques de 1024 bytes
 data = b''
 while True:
     buf = sock.recv(1024)
-    if not buf:
+    if not buf: #Si no queda informacion en el buffer salimos del bucle
         break
     data += buf
 
-sock.close()
-print(data.decode())
+sock.close() #Cerramos el bucle
+print(data.decode())#Decodificamos el mensaje en un formato mas entendible
--- web_Server.py
+++ web_Server.py
@@ -1,32 +1,34 @@
 import socket
 
+# Host local
+SERVER_HOST = '127.0.0.1'
+#Puerto de escucha del servidor
+SERVER_PORT = 8000 
 
-# Define socket host and port
-SERVER_HOST = socket.gethostname()
-SERVER_PORT = 8000
-
-# Create socket
-mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Creamos el socket
+# Especificamos la IPv4 y el protocolo TCP
+mi_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
 mi_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-mi_socket.bind((SERVER_HOST, SERVER_PORT))
-mi_socket.listen(1)
-print('Listening on port %s ...' % SERVER_PORT)
-print("Trying to connect at, http://localhost/8000")
+#Asignamos los recursos
+mi_socket.bind((SERVER_HOST, SERVER_PORT)) 
+#Escuchamos y aceptamos como maximo una peticion
+mi_socket.listen(1) 
+print('Escuchando el puerto %s ...' % SERVER_PORT)
+print("Intentando connectar a, http://localhost/8000")
 while True:    
-    # Wait for client connections
+    # Esperamos a que el cliente se conecte
     client_connection, client_address = mi_socket.accept()
-
-    # Get the client request
+    # Miramos la peticion del cliente
     request = client_connection.recv(1024).decode()
     print(request)
-
+    # Leemos el archivo HTML
     fin = open('htdocs/index.html') 
     content = fin.read()
     fin.close()
-    # Send HTTP response
+    # Enviamos una respuesta al cliente con el contenido HTML
     response = 'HTTP/1.0 200 OK\n\n' + content
     client_connection.sendall(response.encode())
-    client_connection.close()
+    client_connection.close() #Cerramos la conexion
 
 # Close socket
 server_socket.close()
